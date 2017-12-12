f57bb35ceb9cafbc0f91d854b8caf44d77b84cb0
Phil Scott <phil.scott@symphonicsoft.com>
2017-12-12 18:07:22 +0000

    Prefer sets.

--- Day12.hs
+++ Day12.hs
@@ -2,40 +2,43 @@
 
 import Control.Monad.State
 import Data.Attoparsec.Text hiding (takeWhile)
-import Data.List hiding (takeWhile)
+import Data.List hiding (takeWhile, union)
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Maybe
 import qualified Data.Text as T
 
-import Debug.Trace
-
-explore :: Ord a => M.Map a [a] -> a -> State (S.Set a) [a]
+explore :: Ord a => M.Map a (S.Set a) -> a -> State (S.Set a) (S.Set a)
 explore m x = do
   visited <- get
-  let xs = filter (not . (`S.member` visited)) . maybe [] nub . M.lookup x $ m
-  put (visited `S.union` S.fromList xs)
+  let xs = fromMaybe S.empty (M.lookup x m) `S.difference` visited
+  put (visited `S.union` xs)
   pure xs
 
-exploreAll :: Ord a => M.Map a [a] -> [a] -> State (S.Set a) [[a]]
+unions :: Ord a => [S.Set a] -> S.Set a
+unions = foldl S.union S.empty
+
+exploreAll :: Ord a =>
+              M.Map a (S.Set a) -> S.Set a -> State (S.Set a) [S.Set a]
 exploreAll m xs =
-  fmap (xs:) (exploreAll m =<< (fmap concat (traverse (explore m) xs)))
+   fmap (xs:) (exploreAll m
+               =<< (fmap unions (traverse (explore m) . S.toList $ xs)))
 
-orbit :: Ord a => M.Map a [a] -> a -> [a]
-orbit m x =
-  concat . takeWhile (not . null)
-  . evalState (exploreAll m [x]) . S.singleton $ x
+orbit :: Ord a => M.Map a (S.Set a) -> a -> S.Set a
+orbit m x = unions . takeWhile (not . S.null) . evalState (exploreAll m s) $ s
+  where s = S.singleton x
 
-nextOrbit :: (Show a, Ord a) => M.Map a [a] -> S.Set a -> Maybe ([a],S.Set a)
+nextOrbit :: Ord a => M.Map a (S.Set a) -> S.Set a -> Maybe (S.Set a,S.Set a)
 nextOrbit m visited =
-  fmap f (listToMaybe $ S.toList (M.keysSet m `S.difference` visited))
-  where f x = let os = orbit m x in (os, S.fromList os `S.union` visited)
+  fmap f . listToMaybe . S.toList $ (M.keysSet m `S.difference` visited)
+  where f x = let os = orbit m x in (os, os `S.union` visited)
 
-buildMap :: Ord a => (a,[a]) -> M.Map a [a]
+buildMap :: Ord a => (a,[a]) -> M.Map a (S.Set a)
 buildMap (x,ys) =
-  foldl (M.unionWith (++)) (M.singleton x ys) . fmap (`M.singleton` [x]) $ ys
+  foldl (M.unionWith S.union) (M.singleton x (S.fromList ys))
+  . fmap (`M.singleton` (S.singleton x)) $ ys
 
-parsePipe :: Integral a => Parser (M.Map a [a])
+parsePipe :: Integral a => Parser (M.Map a (S.Set a))
 parsePipe = do
   x <- fmap round scientific
   skipSpace
@@ -44,16 +47,16 @@ parsePipe = do
   ys <- sepBy (fmap round scientific) (string ", ")
   pure (buildMap (x,ys))
 
-buildWorld :: Integral a => FilePath -> IO (M.Map a [a])
+buildWorld :: Integral a => FilePath -> IO (M.Map a (S.Set a))
 buildWorld filePath = do
   Right pipes <- fmap (sequence . fmap (parseOnly parsePipe) . T.lines . T.pack)
                  . readFile $ filePath
-  pure (foldl (M.unionWith (++)) M.empty pipes)
+  pure (foldl (M.unionWith S.union) M.empty pipes)
 
 part1 :: FilePath -> IO Int
 part1 filePath = do
   world <- buildWorld filePath
-  pure . length . orbit world $ 0
+  pure . S.size . orbit world $ 0
 
 part2 :: FilePath -> IO Int
 part2 filePath = do
