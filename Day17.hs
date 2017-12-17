7c67e8aeb24a16d416fcd23a99b2824c98e44f57
Phil Scott <phil.scott@symphonicsoft.com>
2017-12-17 18:18:49 +0000

    Initial commit.

---
+++ Day17.hs
@@ -0,0 +1,21 @@
+import Data.List
+
+cycles :: [a] -> [[a]]
+cycles xs = fmap (take (length xs)) (tails (cycle xs))
+
+spinlock :: Int -> [a] -> a -> [a]
+spinlock i xs j = (cycles xs !! i) ++ [j]
+
+cumulate :: Traversable t => (b -> a -> b) -> b -> t a -> t b
+cumulate f = fmap snd . mapAccumL (\acc x -> (f acc x, acc))
+
+part1 :: Integer
+part1 = head $ (cumulate (spinlock 303) [0] [1..]) !! 2017
+
+after0 :: Integral a => a -> a -> a -> [a]
+after0 l' l i | r == 0    = l  : after0 l  (l+1) (r + 1)
+              | otherwise = l' : after0 l' (l+1) (r + 1)
+  where r = (i + 303) `rem` l
+
+part2 :: Integer
+part2 = after0 1 1 0 !! 50000000
